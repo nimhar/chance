7a6522a980558c9cf23ef6fe8da0f9c39028b4f8
Nimrod Harel <nimrodharel@Nimrods-MacBook-Pro.local>
2025-01-29 15:58:45 -0800

    Fix GitHub Pages routing and update page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import CreateLottery from './pages/CreateLottery'
 import ViewLottery from './pages/ViewLottery'
 import SingleTaskLottery from './pages/SingleTaskLottery'
 import Header from './components/Header'
+import './App.css'
 
 function App() {
   return (
     <ChakraProvider>
-      <Router>
+      <Router basename="/chance">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/create" replace />} />
           <Route path="/create" element={<CreateLottery />} />
           <Route path="/lottery/:id" element={<ViewLottery />} />
           <Route path="/single-lottery/:id" element={<SingleTaskLottery />} />
